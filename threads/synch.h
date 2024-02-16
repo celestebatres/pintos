378daa6e8e889215b413c04548a93b326ca18ea0
DiegoSican <68403313+DiegoSican@users.noreply.github.com>
2024-02-16 15:09:10 -0600

    Fase 1

--- threads/synch.h
+++ threads/synch.h
@@ -5,10 +5,11 @@
 #include <stdbool.h>
 
 /* A counting semaphore. */
-struct semaphore 
+struct semaphore
   {
     unsigned value;             /* Current value. */
     struct list waiters;        /* List of waiting threads. */
+    int priority;               /* Las prioridades del semaforo */
   };
 
 void sema_init (struct semaphore *, unsigned value);
@@ -18,10 +19,12 @@ void sema_up (struct semaphore *);
 void sema_self_test (void);
 
 /* Lock. */
-struct lock 
+struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+    struct list_elem lockelem;  /* Lista de los elementos para el bloqueo del thread */
+    int priority;               /* Prioridad del thread que lo mantiene bloqueado, clasificado por donacion */
   };
 
 void lock_init (struct lock *);
@@ -31,7 +34,7 @@ void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
 
 /* Condition variable. */
-struct condition 
+struct condition
   {
     struct list waiters;        /* List of waiting threads. */
   };
