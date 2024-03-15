a8aaa7b58f215bf699bdbcf8d181d83586fc97a8
MonicaPar <68394605+MonicaPar@users.noreply.github.com>
2024-03-15 00:46:58 -0600

    Update process.h

--- userprog/process.h
+++ userprog/process.h
@@ -2,6 +2,26 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
+#include "threads/synch.h"
+
+/* elemento de file_d_list */
+struct file_d {
+  int id;
+  struct list_elem elem;
+  struct file* file;
+};
+
+struct process_control_block {
+  tid_t pid;
+  const char* cmdline;
+  struct list_elem elem;
+  bool parent_wait;
+  bool curr_finish;
+  int exit_code;
+  struct semaphore inicio;
+  struct semaphore tiempo_padre;
+  bool libera_hijo;
+};
 
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
